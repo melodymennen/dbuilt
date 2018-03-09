dcf31217f076e508b16d598401ce57ebac0349ec
Melody <33438150+melodymennen@users.noreply.github.com>
2018-03-08 18:24:37 -0700

    added to header

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -10,8 +10,24 @@ const Header = () => {
                     <div className="header_links">log in</div>
                 </div>
             </div>
-            <div className="header_"></div>
-            <div className="header_"></div>
+            <div className="header_main">
+                <div className="header_main-middle">
+                    <div className="header_main-middle-left">
+                        <div>logo</div>
+                        <div>search bar</div>
+                    </div>
+                    <div className="header_main-middle-right">
+                        <div>number</div>
+                        <div>contact us</div>
+                        <div>cart</div>
+                    </div>
+                </div>
+                <div className="header_main-bottom"></div>
+            </div>
+            <div className="header_mobile">
+                <div className="header_mobile-left"></div>
+                <div className="header_mobile-right"></div>
+            </div>
         </div>
     )
 }
